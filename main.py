340e7e68001ed06a3a74b099d50ee2b604e79832
KYEONGMIN CHO <ggingmin@gmail.com>
2023-04-07 14:24:54 +0900

    first

--- main.py
+++ main.py
@@ -29,35 +29,44 @@ def test(message, say):
 
 
 @app.event("message")
-def handle_message(message, say):
+def handle_direct_message(message, say):
+
+    start_sequence = "\nAI:"
+    restart_sequence = "\nHuman: "
 
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"{message}",
-        temperature=0,
+        prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {message}",
+        temperature=0.9,
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n"]
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
     )
     
     print(response)
     say(response.choices[0].text)
 
-# @app.event("app_mention")
-# def answer(message, say):
-#     response = openai.Completion.create(
-#         model="text-davinci-003",
-#         prompt=f"클라우드타입 ChatGPT 슬랙봇 Python 템플릿입니다. 질문을 입력해주세요.\n>> {message}",
-#         max_tokens=50,
-#         temperature=0,
-#         top_p=1,
-#         stop=["\n"]
-#     )
+@app.event("app_mention")
+def handle_mention(message, say):
+    
+    start_sequence = "\nAI:"
+    restart_sequence = "\nHuman: "
 
-#     print(response)
-#     say(response.choices[0])
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {message}",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    
+    print(response)
+    say(response.choices[0].text)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
