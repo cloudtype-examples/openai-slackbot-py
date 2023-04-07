e41575821cb9a2e50910ab62cde848ded4392c2c
KYEONGMIN CHO <ggingmin@gmail.com>
2023-04-07 14:32:45 +0900

    edited

--- main.py
+++ main.py
@@ -21,12 +21,6 @@ def test(message, say):
     user = message['user']
     say(f"{user} 님, ChatGPT 테스트입니다.")
 
-# @app.event("app_mention")
-# def test(message, say):
-#     print(message)
-#     user = message['user']
-#     say(f"{user} 님, ChatGPT 테스트입니다.")
-
 
 @app.event("message")
 def handle_direct_message(message, say):
@@ -38,7 +32,7 @@ def handle_direct_message(message, say):
         model="text-davinci-003",
         prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {message}",
         temperature=0.9,
-        max_tokens=150,
+        max_tokens=500,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
@@ -58,7 +52,7 @@ def handle_mention(message, say):
         model="text-davinci-003",
         prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {message}",
         temperature=0.9,
-        max_tokens=150,
+        max_tokens=500,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
